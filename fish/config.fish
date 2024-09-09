32171832e5d8f3ffd57c65e1fdf0ad380622d3e4
Luke Barkess <luke.barkess@icloud.com>
2024-09-10 00:46:17 +0100

    laptop updates

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,8 @@
 function fish_greeting
     if status is-interactive
-        if test $TERM != linux
+        if test "$TERM" != "linux"
             starship init fish | source
-
             pfetch
-
             echo Welcome (set_color blue; echo -n $USER; set_color normal) to (set_color green; echo -n $hostname; set_color normal)
         end
     end
@@ -14,5 +12,3 @@ set -x PATH $HOME/.cargo/bin $PATH
 
 # Created by `pipx` on 2023-06-20 20:54:03
 set PATH $PATH /home/brumus/.local/bin
-
-alias hx='helix'
--- nvim/lua/plugins/notifier.lua
+++ nvim/lua/plugins/notifier.lua
@@ -1,7 +1,7 @@
 return {
     "vigoux/notifier.nvim",
     opts = {
-        status_width = 999,
+        -- status_width = 999,
         notify = {
             clear_time = 10000,
         },
