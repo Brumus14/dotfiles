e60400cf605f1f90b75ff6c813f8c56eed9fb1a8
Brumus14 <luke.barkess@icloud.com>
2024-09-26 01:13:39 +0100

    clang-format update

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -4,10 +4,17 @@ return {
         formatters_by_ft = {
             lua = { "stylua" },
             rust = { "rustfmt" },
+            c = { "clang_format" },
+            cpp = { "clang_format" },
         },
         formatters = {
             stylua = {
                 prepend_args = { "--indent-type", "Spaces" },
+            },
+            clang_format = {
+                prepend_args = {
+                    "--style={IndentWidth: 4, AllowShortFunctionsOnASingleLine: None, SortIncludes: false}",
+                },
             },
         },
     },
