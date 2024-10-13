15cb734eb37a732ac4c61149572e760541310bb4
Luke Barkess <luke.barkess@icloud.com>
2024-10-13 02:19:15 +0100

    automatic monitor configs neovim stuff

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -1,3 +1,10 @@
 vim.api.nvim_set_keymap("n", "<C-Space>", ":Oil<CR>", { noremap = true })
 
 vim.keymap.set("n", "K", require("hover").hover, { desc = "hover.nvim" })
+
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
+
+vim.keymap.set("n", "<C-i>", "<Cmd>nohlsearch|diffupdate|normal! <C-L><CR>")
